0d34191f66e1cd8113bf0b478d7dd6e030471799
James Byars <jbyars4ku@gmail.com>
2014-09-30 20:55:23 -0500

    Initial Commit

---
+++ MultiActivity/src/com/example/multiactivity/ActivityTwo.java
@@ -0,0 +1,16 @@
+package com.example.multiactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_two);
+	}
+	
+	
+	
+	
+}
